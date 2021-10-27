2478f25b4e3b2a98943d471ae5b4679a1db470d6
Remington Rohel <remington.rohel@usask.ca>
2021-10-27 12:20:54 -0600

    Created exception class for upstream conversion failure.

--- exceptions/conversion_exceptions.py
+++ exceptions/conversion_exceptions.py
@@ -4,3 +4,34 @@
 This files contains the exceptions generated when an impossible
 conversion is attempted.
 """
+
+import logging
+postprocessing_logger = logging.getLogger('borealis_postprocessing')
+
+
+class ConversionUpstreamError(Exception):
+    """
+    Raised when the file cannot be converted because the desired
+    conversion is upstream.
+
+    Parameters
+    ----------
+    error_str: str
+        explanation for why the file cannot be converted.
+
+    Attributes
+    ----------
+    message: str
+        The message to display with the error
+
+    See Also
+    --------
+    conversion.py
+    """
+
+    def __init__(self, error_str: str):
+        self.message = "The file cannot be converted due to the "\
+            " following error: {error_str}"\
+            "".format(error_str=error_str)
+        postprocessing_logger.error(self.message)
+        Exception.__init__(self, self.message)
