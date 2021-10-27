94ac5374f4736b3a28416a07f5241b059098ead5
Remington Rohel <remington.rohel@usask.ca>
2021-10-27 12:12:59 -0600

    Created file for holding exceptions raised by impossible conversion attempt.

---
+++ exceptions/conversion_exceptions.py
@@ -0,0 +1,6 @@
+# Copyright 2021 SuperDARN Canada, University of Saskatchewan
+# Author: Remington Rohel
+"""
+This files contains the exceptions generated when an impossible
+conversion is attempted.
+"""
