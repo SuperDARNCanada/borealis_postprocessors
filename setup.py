58f7a4d363db0a875371348d5f8a521712cda540
Remington Rohel <remington.rohel@usask.ca>
2022-10-03 16:13:36 -0600

    Added in some args to setup() so that it will build correctly on legacy systems.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='borealis-postprocessors',
+    version='1.0.0',
+    description='Post-process SuperDARN Borealis data files',
+    author='Remington Rohel',
+    license='GPLv3',
+    requires=['numpy', 'scipy', 'pyDARNio', 'h5py', 'deepdish']
+)
