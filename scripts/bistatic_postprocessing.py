cb419190e9697e786154443ebcc3addd6e83c0fb
Remington Rohel <remington.rohel@usask.ca>
2022-10-04 14:29:18 -0600

    Removed conversion to SuperDARN format as pyDARNio doesn't have multi-beam support in all branches.

--- scripts/bistatic_postprocessing.py
+++ scripts/bistatic_postprocessing.py
@@ -5,7 +5,7 @@ import h5py
 import deepdish as dd
 from datetime import datetime
 
-from postprocessors import ConvertFile, borealis_to_sdarn_rename, borealis_to_borealis_rename
+from postprocessors import ConvertFile, borealis_to_borealis_rename
 from postprocessors.sandbox.widebeam_antennas_iq_to_bfiq import ProcessWidebeamAntennasIQ2Bfiq
 
 
@@ -40,27 +40,19 @@ def main(in_directory: str, out_directory: str, search_pattern: str):
 
         bfiq_file = borealis_to_borealis_rename(filename, 'bfiq', 'site')
         rawacf_file = borealis_to_borealis_rename(filename, 'rawacf', 'site')
-        sdarn_file = borealis_to_sdarn_rename(filename, 'rawacf')
 
         bfiq_path = f'{out_directory}/{bfiq_file}'
         rawacf_path = f'{out_directory}/{rawacf_file}'
-        sdarn_path = f'{out_directory}/{sdarn_file}'
 
         start = datetime.utcnow()
 
         if os.path.isfile(rawacf_path):   # Just need to convert to SuperDARN format
-            if os.path.isfile(sdarn_path):  # Already processed this file
-                print(f'{path} - Already done. ', end='')
-            else:
-                print(f'{path} - Converting {rawacf_file} to SuperDARN format. ', end='')
-                ConvertFile(rawacf_path, sdarn_path, "rawacf", 'rawacf', 'site', 'dmap')
+            print(f'{path} - Already done. ', end='')
 
         elif os.path.isfile(bfiq_path):
             # bfiq already exists
             print(f'{bfiq_path} -> {rawacf_file}  ', end='')
             ConvertFile(bfiq_path, rawacf_path, 'bfiq', 'rawacf', 'site', 'site', averaging_method)
-            if not os.path.isfile(sdarn_path):
-                ConvertFile(rawacf_path, sdarn_path, 'rawacf', 'rawacf', 'site', 'dmap', averaging_method)
             os.remove(bfiq_path)       # Don't want to keep this around
 
         else:
@@ -78,22 +70,17 @@ def main(in_directory: str, out_directory: str, search_pattern: str):
                     experiment_comment = attributes['experiment_comment']
 
             if experiment_name in ['Widebeam_2tx', 'Widebeam_3tx', 'MultifreqWidebeam']:    # These ones need some love
-                print(f'{path} -> {sdarn_file}  ', end='')
+                print(f'{path} -> {rawacf_file}  ', end='')
                 ProcessWidebeamAntennasIQ2Bfiq(path, bfiq_path, input_structure, 'site')
                 ConvertFile(bfiq_path, rawacf_path, 'bfiq', 'rawacf', 'site', 'site', averaging_method)
-                if not os.path.isfile(sdarn_path):
-                    ConvertFile(rawacf_path, sdarn_path, 'rawacf', 'rawacf', 'site', 'dmap', averaging_method)
                 os.remove(bfiq_path)    # We don't need to keep these around
 
             elif experiment_name == 'BistaticTest':
                 if 'Bistatic widebeam mode' in experiment_comment:
                     print(f'{path} - Bistatic listening experiment, cannot process.  ', end='')
                 else:
-                    # Convert to SuperDARN-formatted rawacf file
-                    print(f'{path} -> {sdarn_file}  ', end='')
+                    print(f'{path} -> {rawacf_file}  ', end='')
                     ConvertFile(path, rawacf_path, 'antennas_iq', 'rawacf', input_structure, 'site', averaging_method)
-                    if not os.path.isfile(sdarn_path):  # Already processed this file
-                        ConvertFile(rawacf_path, sdarn_path, 'rawacf', 'rawacf', 'site', 'dmap', averaging_method)
             else:
                 print(f'{path} - Not a widebeam experiment.  ', end='')
 
