e1e30773faf3e4f5756b9badff75dbf0cad2d529
Remington Rohel <rar129@usask.ca>
2023-08-12 13:43:33 -0600

    Added plotting for the wavelet data.

--- scripts/wavelet_analysis.py
+++ scripts/wavelet_analysis.py
@@ -2,7 +2,8 @@ import argparse
 import glob
 import os
 import h5py
-import deepdish as dd
+import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 from postprocessors import ConvertFile, borealis_to_borealis_rename
@@ -31,13 +32,31 @@ def main(infile: str):
             input_structure = "array"
 
         filename = os.path.basename(path)
-
-        outfile = borealis_to_borealis_rename(filename, 'wavelet', 'site')
+        dirname = os.path.dirname(path)
+        outfile = dirname + '/' + borealis_to_borealis_rename(filename, 'wavelet', 'site')
 
         start = datetime.utcnow()
 
-        WaveletDecomposition(path, outfile, input_structure)
+        # WaveletDecomposition(path, outfile, input_structure)
 
+        with h5py.File(outfile, 'r') as f:
+            recs = sorted(list(f.keys()))
+            rec = f[recs[0]]
+
+            fig, axes = plt.subplots(6, 1, figsize=(10, 8))
+            axes[0].plot(10 * np.log10(np.abs(rec['data'][:].reshape(rec['data_dimensions'][:])[10, 10].real)))
+            axes[1].plot(10 * np.log10(np.abs(rec['detail1'][10, 10].real)))
+            axes[2].plot(10 * np.log10(np.abs(rec['detail2'][10, 10].real)))
+            axes[3].plot(10 * np.log10(np.abs(rec['detail3'][10, 10].real)))
+            axes[4].plot(10 * np.log10(np.abs(rec['detail4'][10, 10].real)))
+            axes[5].plot(10 * np.log10(np.abs(rec['approx'][10, 10].real)))
+            axes[0].plot(10 * np.log10(np.abs(rec['data'][:].reshape(rec['data_dimensions'][:])[10, 10].imag)))
+            axes[1].plot(10 * np.log10(np.abs(rec['detail1'][10, 10].imag)))
+            axes[2].plot(10 * np.log10(np.abs(rec['detail2'][10, 10].imag)))
+            axes[3].plot(10 * np.log10(np.abs(rec['detail3'][10, 10].imag)))
+            axes[4].plot(10 * np.log10(np.abs(rec['detail4'][10, 10].imag)))
+            axes[5].plot(10 * np.log10(np.abs(rec['approx'][10, 10].imag)))
+            plt.show()
         end = datetime.utcnow()
         duration = (end - start).total_seconds()
         print(f'Time: {duration:.2f} s')
