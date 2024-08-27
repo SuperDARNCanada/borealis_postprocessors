15ec3f2621f22e1262cebc9427c74181b832e64c
Remington Rohel <rar129@usask.ca>
2024-08-27 14:58:56 +0000

    Added script and processing class for updating WAL beam directions.
    * Class will reprocess antennas_iq -> rawacf, with a new beam direction reflecting the beamforming that was done when the radar ran
    * Script will go through HDF5 and DMAP files, updating the beam direction metadata to the correct beam directions.
    * Also removed setup.py file that was unnecessary, and updated minimum Python version in pyproject.toml

---
+++ postprocessors/sandbox/update_beam_dirs.py
@@ -0,0 +1,87 @@
+"""
+This file contains a class to modify the beam_azms metadata of antennas_iq files and reprocess them to rawacf.
+"""
+
+from collections import OrderedDict
+import numpy as np
+from postprocessors import AntennasIQ2Rawacf
+from postprocessors.core.antennas_iq_to_bfiq import radar_dict
+
+
+class UpdateBeamDirs(AntennasIQ2Rawacf):
+    """
+    Class for conversion of Borealis antennas_iq files into rawacf files with new beam directions.
+
+    Attributes
+    ----------
+    infile: str
+        The filename of the input antennas_iq file.
+    outfile: str
+        The file name of output file
+    infile_structure: str
+        The structure of the file. Structures include:
+        'array'
+        'site'
+    outfile_structure: str
+        The desired structure of the output file. Same structures as above, plus 'dmap'.
+    """
+
+    old_spacing = radar_dict["sas"]["main_antenna_spacing"]
+    new_spacing = radar_dict["wal"]["main_antenna_spacing"]
+
+    def __init__(
+        self,
+        infile: str,
+        outfile: str,
+        infile_structure: str,
+        outfile_structure: str,
+        **kwargs,
+    ):
+        """
+        Initialize the attributes of the class.
+
+        Parameters
+        ----------
+        infile: str
+            Path to input file.
+        outfile: str
+            Path to output file.
+        infile_structure: str
+            Borealis structure of input file. Either 'array' or 'site'.
+        outfile_structure: str
+            Borealis structure of output file. Either 'array', 'site', or 'dmap'.
+        """
+        super().__init__(infile, outfile, infile_structure, outfile_structure)
+
+    def process_record(cls, record: OrderedDict, **kwargs) -> OrderedDict:
+        """Update the beam_azms field, then process normally"""
+
+        old_azms = record["beam_azms"]
+        if isinstance(old_azms, list):
+            new_azms = [
+                np.round(
+                    np.rad2deg(
+                        np.arcsin(
+                            cls.old_spacing
+                            * np.sin(np.deg2rad(theta))
+                            / cls.new_spacing
+                        )
+                    ),
+                    decimals=2,
+                )
+                for theta in old_azms
+            ]
+        else:
+            new_azms = np.round(
+                np.rad2deg(
+                    np.arcsin(
+                        cls.old_spacing * np.sin(np.deg2rad(old_azms)) / cls.new_spacing
+                    )
+                ),
+                decimals=2,
+            )
+
+        record["beam_azms"] = new_azms
+        record = super().process_record(record, **kwargs)
+
+        return record
---
+++ scripts/wal_beam_update.py
@@ -0,0 +1,146 @@
+"""
+Updates the beam_azms metadata in antennas_iq and rawacf files to reflect the correct beam directions.
+"""
+
+import argparse
+import glob
+import os
+
+import dmap  # `pip install darn-dmap`
+import h5py
+import numpy as np
+
+from postprocessors.sandbox.update_beam_dirs import UpdateBeamDirs
+from postprocessors import borealis_to_borealis_rename
+
+
+old_spacing = 15.24  # meters
+new_spacing = 12.8016  # meters
+
+
+def hdf5_fixer(path: str):
+    structure = get_structure(path)
+    if structure == "dmap":
+        raise ValueError(f"Unknown structure for file {os.path.basename(path)}")
+
+    with h5py.File(path, "r+") as f:
+        if structure == "array":
+            new_beam_dirs = np.round(
+                np.rad2deg(
+                    np.arcsin(
+                        old_spacing
+                        * np.sin(np.deg2rad(f["beam_azms"][:]))
+                        / new_spacing
+                    )
+                ),
+                decimals=2,
+            )
+            f["beam_azms"][:, :] = new_beam_dirs
+        else:
+            for name in sorted(list(f.keys())):
+                rec = f[name]
+                new_beam_dirs = np.round(
+                    np.rad2deg(
+                        np.arcsin(
+                            old_spacing
+                            * np.sin(np.deg2rad(rec["beam_azms"][:]))
+                            / new_spacing
+                        )
+                    ),
+                    decimals=2,
+                )
+                rec["beam_azms"][:] = new_beam_dirs
+    print("  Fixed HDF5 file")
+
+
+def get_structure(path: str):
+    if path.endswith("site"):
+        return "site"
+    if path.endswith("hdf5"):
+        return "array"
+    return "dmap"
+
+
+def dmap_fixer(path: str):
+    """Updates the bmazm metadata in a DMAP rawacf file"""
+    data = dmap.read_rawacf(path)
+    for rec in data:
+        rec["bmazm"] = np.round(
+            np.rad2deg(
+                np.arcsin(old_spacing * np.sin(np.deg2rad(rec["bmazm"])) / new_spacing)
+            ),
+            decimals=2,
+        )
+    dmap.write_rawacf(data, path)
+    print("  Fixed DMAP file")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="Directory with files to process")
+    parser.add_argument(
+        "--pattern", help="Pattern to match filenames", default="*hdf5*"
+    )
+    parser.add_argument(
+        "--reprocess",
+        type=bool,
+        default=False,
+        action="store_true",
+        help="Flag to reprocess the antennas_iq files to rawacf, rather than just update the metadata",
+    )
+    parser.add_argument(
+        "--for-real",
+        type=bool,
+        action="store_true",
+        help="Flag to modify the files (otherwise, just logs what the script *would* do)",
+    )
+    args = parser.parse_args()
+
+    files = glob.glob(f"{args.dir}/{args.pattern}")
+    num_files = len(files)
+    failed_files = []
+
+    for i, infile in enumerate(files):
+        print(f"{i:>5d}/{num_files}  {infile}", end="")
+
+        try:
+            if "antennas_iq" in infile:
+                if args.for_real:
+                    if args.reprocess:
+                        processor = UpdateBeamDirs(
+                            infile,
+                            borealis_to_borealis_rename(infile, "rawacf", "site"),
+                            get_structure(infile),
+                            "site",
+                        )
+                        processor.process_file(num_processes=5)
+                    else:
+                        hdf5_fixer(infile)
+                else:
+                    if args.reprocess:
+                        print("  Reprocessing antennas_iq file")
+                    else:
+                        print("  Fixing antennas_iq file")
+
+            if "rawacf" in infile:
+                if get_structure(infile) == "dmap":
+                    if args.for_real:
+                        dmap_fixer(infile)
+                    else:
+                        print("  Fixing rawacf DMAP file")
+                else:
+                    if args.for_real:
+                        hdf5_fixer(infile)
+                    else:
+                        print("  Fixing rawacf HDF5 file")
+
+            else:
+                raise ValueError(
+                    f"Unknown file type for file: {os.path.basename(infile)}"
+                )
+        except:
+            failed_files.append(infile)
+
+    print(f"\n\nUnable to fix {len(failed_files)} files")
+    for failed_file in failed_files:
+        print(f"\t{failed_file}")
