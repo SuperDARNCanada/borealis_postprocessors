0260e8050da76afafc1e5781ead35a6f51e10aa8
Remington Rohel <rar129@usask.ca>
2025-11-21 17:39:41 +0000

    Fixed compare_files for mismatched array shapes (flattens).
    * Removed setup.py body since redundant with pyproject.toml

--- test/utils/compare_files.py
+++ test/utils/compare_files.py
@@ -45,12 +45,14 @@ def compare_files(file1, file2):
             else:
                 # Compare floating-point values differently
                 if 'float' in str(entry1.dtype) or 'complex' in str(entry1.dtype):
-                    if not np.allclose(entry1[()].flatten(), entry2[()].flatten(), equal_nan=True):
+                    lhs = entry1[()].flatten()
+                    rhs = entry2[()].flatten()
+                    if not np.allclose(lhs, rhs, equal_nan=True):
                         compare_string += prefix + f"/{key}:\n" \
                                                    f"\t{entry1}\n" \
                                                    f"\t{entry2}\n" \
                                                    f"\tDifference: " \
-                                                   f"{np.nanmax(np.abs((entry1[()] - entry2[()])/entry1[:]))}\n"
+                                                   f"{np.nanmax(np.abs((lhs - rhs) / lhs))}\n"
                 # Comparing non-floating-point values
                 elif not np.array_equal(entry1, entry2):
                     compare_string += prefix + f"/{key}:\n" \
