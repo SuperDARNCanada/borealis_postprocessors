c811c983dd1945167bb7c5a9c0a1d679a21b4093
Remington Rohel <rar129@usask.ca>
2025-11-26 21:18:43 +0000

    Removed multiprocessing in near_range.py

--- scripts/near_range.py
+++ scripts/near_range.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     parser.add_argument('outdir', help='Directory to store processed files in')
     parser.add_argument('--pattern', help='Pattern to search for when globbing files from indir',
                         default='*antennas_iq.h5')
-    parser.add_argument('--dmap', help='Process files to DMAP?', action='store_true')
     args = parser.parse_args()
     in_directory = args.indir
     out_directory = args.outdir
@@ -41,7 +40,7 @@ if __name__ == '__main__':
             if not os.path.isfile(rawacf_path):
                 print(f'\t-> {rawacf_path}')
                 processor = NearRange(path, rawacf_path, input_structure, "dmap")
-                processor.process_file(num_processes=4, keep_intermediate_files=False)
+                processor.process_file(keep_intermediate_files=False)
         except Exception as e:
             print(f"{path} failed:\n{e}")
             continue
