fixes to accomodate different versions and the widebeam and rawacf averaging merge to develop

postprocessors/core/restructure.py
@@ -194,7 +194,15 @@ def write_records(hdf5_file: h5py.File, records: dict, version=(0, 5)):
                             dset.attrs['strtype'] = b'unicode'
                             dset.attrs['itemsize'] = v.dtype.itemsize // 4  # every character is 4 bytes
                         else:
-                            group.create_dataset(k, data=np.bytes_(v))
+                            # group.create_dataset(k, data=np.bytes_(v))
+                            if 'descriptors' in k:  # This is to work around splicing error as np.bytes_(v) when v is 'data_descriptors' fails
+                                v_dum = []
+                                for d in v:
+                                    v_dum.append(np.bytes_(d))
+                                v_dum = np.array(v_dum)
+                                group.create_dataset(k, data=v_dum)
+                            else:
+                                group.create_dataset(k, data=np.bytes_(v))
                     else:
                         group.create_dataset(k, data=v)
                 else:
@@ -322,7 +330,6 @@ def convert_to_numpy(data: dict, version=(0, 5)):
                 data[k] = np.array(v)
         else:
             continue
-
     return data
 
 

test/utils/compare_files.py
@@ -50,7 +50,7 @@ def compare_files(file1, file2):
                                                    f"\t{entry1}\n" \
                                                    f"\t{entry2}\n" \
                                                    f"\tDifference: " \
-                                                   f"{np.nanmax(np.abs((entry1[()] - entry2[()])/entry1[:]))}\n"
+                                                   f"{np.nanmax(np.abs((entry1[()].flatten() - entry2[()].flatten())/entry1[:].flatten()))}\n"
                 # Comparing non-floating-point values
                 elif not np.array_equal(entry1, entry2):
                     compare_string += prefix + f"/{key}:\n" \